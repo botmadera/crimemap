5b0dab00ca2a5639a65a91fbda4c69cb6f748c21
Bot Madera <botmadera@gmail.com>
2017-04-04 21:05:43 -0400

    Skeleton CrimeMap

---
+++ crimemap.py
@@ -0,0 +1,28 @@
+from dbhelper import dbhelper
+from flask import flask
+from flask import render_template
+
+app = Flask(__name__)
+
+DB = DBHelper()
+
+@app.route("/")
+def home():
+    try:
+        data = DB.get_all_inputs()
+
+    except Exception as e:
+        print e
+        data = None
+    return render_template("home.html", data = data)
+
+@app.route("/clear")
+def clear():
+    try:
+        DB.clear_all()
+    except Exception as e:
+        print e
+    return home()
+
+if __name__ == '__main__':
+    app.run()
