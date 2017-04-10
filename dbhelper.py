561d0c1ebf2ce59a9d17abadd466cb5e1698d9e3
Bot Madera <botmadera@gmail.com>
2017-04-09 21:37:28 -0400

    d

--- dbhelper.py
+++ dbhelper.py
@@ -19,7 +19,7 @@ class DBHelper:
         connection = self.connect()
         try:
         # The following introduces a deliberate security flaw. See section on SQL injection below
-        query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES('{}');".format(data)
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 connection.commit()
